78b59c81e3ea314fd52f5283e09d8fb777784425
SriramSohanBasa <basa.s@northeastern.edu>
2024-09-17 14:02:32 -0400

    Tennis_analysis-v2

---
+++ main.py
@@ -0,0 +1,16 @@
+from utils import (read_video, 
+                   save_video,
+                   measure_distance,
+                   draw_player_stats,
+                   convert_pixel_distance_to_meters
+                   )
+
+def main():
+    # Read Video
+    input_video_path = "input_videos/input_video.mp4"
+    video_frames = read_video(input_video_path)
+    
+    save_video(video_frames, "output_videos/output_video.avi")
+
+if __name__ == "__main__":
+    main()
