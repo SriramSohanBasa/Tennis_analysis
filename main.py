571580e258e948bf1b2d80d13e881c98a8237e39
SriramSohanBasa <basa.s@northeastern.edu>
2024-10-16 20:50:46 -0400

    added player_tracker_v1

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 from utils import (read_video, 
-                   save_video,
-                   measure_distance,
-                   draw_player_stats,
-                   convert_pixel_distance_to_meters
+                   save_video
+                #    measure_distance,
+                #    draw_player_stats,
+                #    convert_pixel_distance_to_meters
                    )
 
+
 def main():
+
     # Read Video
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
